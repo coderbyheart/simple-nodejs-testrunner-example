a59745c42c95fa465d21d3f0d9fce19622b40842
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-03-15 00:13:34 +0100

    Example

---
+++ tests/a.test.js
@@ -0,0 +1,4 @@
+import { it } from "node:test";
+import assert from "node:assert/strict";
+
+it("should run this test", () => assert.equal(1, 1));
---
+++ tests/b.test.js
@@ -0,0 +1,4 @@
+import { it } from "node:test";
+import assert from "node:assert/strict";
+
+it("should run this test as well", () => assert.equal(2, 2));
